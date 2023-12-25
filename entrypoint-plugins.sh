635713218c70b61ea51bdfdc10b27c3a156adae3
Max Goltzsche <max.goltzsche@gmail.com>
2023-12-25 12:39:08 +0100

    feat: define genres, assign up to 2 per track
    Add a default genre whitelist which can be adjusted manually within the library directory.
    The list should be kept in sync with the playlist generation rules: each genre that is used within a playlist generation rule should be registered explicitly within the genre whitelist.

--- entrypoint-plugins.sh
+++ entrypoint-plugins.sh
@@ -2,3 +2,4 @@
 
 mkdir -p /tmp/xtractor
 [ -f "$BEETSDIR/config.yaml" ] || cp /etc/beets/default-config.yaml "$BEETSDIR/config.yaml"
+[ -f "$BEETSDIR/genres.txt" ] || cp /etc/beets/default-genres.txt "$BEETSDIR/genres.txt"
